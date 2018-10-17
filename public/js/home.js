b2d2475f5846810460d67e9eee5bd6dd7917ff33
tomellericcardo <tomelleri.riccardo@gmail.com>
2018-10-17 23:09:02 +0200

    add 'imposta corso' feature

--- public/js/home.js
+++ public/js/home.js
@@ -2,37 +2,82 @@ var home = {
 
     init: function() {
         home.init_ripple();
+        note.init_dialog();
+        note.init_select();
+        note.init_imposta();
+        home.init_corso();
+        home.init_imposta();
         home.init_orario();
     },
 
     init_ripple: function() {
-        var elements = $('.mdc-list-item');
+        var elements = $('.mdc-list-item, .mdc-button');
         for (var i = 0; i < elements.length; i++)
             mdc.ripple.MDCRipple.attachTo(elements[i]);
     },
 
+    init_dialog: function() {
+        var element = $('.mdc-dialog')[0];
+        home.dialog = mdc.dialog.MDCDialog.attachTo(element);
+    },
+
+    init_select: function() {
+        var elements = $('.mdc-select');
+        for (var i = 0; i < elements.length; i++)
+            mdc.select.MDCSelect.attachTo(elements[i]);
+        var url = 'https://logistica.univr.it/aule/Orario/index.php?view=easycourse&include=corso&_lang=it'
+        $.get(url, function(contenuto) {
+            var corsi = $(contenuto).filter('#cdl_co').html();
+            $('#corso').html(corsi);
+        });
+        $('#corso').on('change', function() {
+            $.get(url + '&corso=' + $('#corso').val(), function(contenuto) {
+                var anni = $(contenuto).filter('#cdl_a2').html();
+                $('#anno').html(anni);
+            });
+        });
+    },
+
+    init_imposta: function() {
+        $('#imposta').on('click', function() {
+            var corso = $('#corso').val();
+            var anno = $('#anno').val();
+            if (corso && anno)
+                localStorage.setItem('corso', {
+                    corso: corso,
+                    anno: anno
+                });
+        });
+    },
+
+    init_corso: function() {
+        home.corso = localStorage.getItem('corso');
+        if (!home.corso)
+            home.dialog.open();
+    },
+
     init_orario: function() {
         $('#questa_settimana').on('click', function () {
-            home.questa_settimana();
+            home.orario();
         });
         $('#prossima_settimana').on('click', function () {
-            home.prossima_settimana();
+            home.orario(settimana = true);
         });
         $('#insegnamento').on('click', function () {
             window.location.href = 'https://logistica.univr.it/aule/Orario/?view=easycourse&include=attivita&_lang=it';
         });
     },
 
-    questa_settimana: function() {
-        var url = 'https://logistica.univr.it/aule/Orario/?list=0&anno=2018&corso=338&anno2=779%7C1&visualizzazione_orario=cal&view=easycourse&include=corso&_lang=it&col_cells=0&date=';
-        url +=  home.crea_data(false);
-        window.location.href = url;
-    },
-
-    prossima_settimana: function() {
-        var url = 'https://logistica.univr.it/aule/Orario/?list=0&anno=2018&corso=338&anno2=779%7C1&visualizzazione_orario=cal&view=easycourse&include=corso&_lang=it&col_cells=0&date=';
-        url +=  home.crea_data(true);
-        window.location.href = url;
+    orario: function(settimana = false) {
+        if (!home.corso)
+            home.dialog.open();
+        else {
+            var url = 'https://logistica.univr.it/aule/Orario/?list=0&anno=2018&visualizzazione_orario=cal&view=easycourse&include=corso&_lang=it&col_cells=0';
+            url += '&corso=' + home.corso.corso;
+            url += '&anno2=' + home.corso.anno;
+            url += '&date=' + home.crea_data(settimana);
+            window.location.href = url;
+        }
     },
 
     crea_data: function(settimana) {
