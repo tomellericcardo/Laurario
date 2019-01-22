410d7b75a2792f1f078774e9eb345a0279c3cd49
tomellericcardo <tomelleri.riccardo@gmail.com>
2019-01-22 13:42:54 +0100

    fix cache updating bug

--- service-worker.js
+++ service-worker.js
@@ -47,6 +47,7 @@ function fromCache(request) {
 }
 
 function update(request) {
+    if (request.cache === 'only-if-cached' && request.mode !== 'same-origin') return;
     return caches.open(CACHE).then(function(cache) {
         return fetch(request).then(function(response) {
             return cache.put(request, response);
